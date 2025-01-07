0f66f56b6df14080b648773aa06ae754d5984588
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2025-01-07 19:02:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+
+    int countPairs(int arr[], int target) {
+        // Complete the function
+        int n = arr.length, ans = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i = 0; i < n; i++){
+            int req = target-arr[i];
+            ans += hm.getOrDefault(req, 0);
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0)+1);
+        }
+        return ans;
+    }
+}
+
+
+//{ Driver Code Starts.
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            String[] inputLine = br.readLine().trim().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            inputLine = br.readLine().trim().split(" ");
+            int target = Integer.parseInt(inputLine[0]);
+
+            Solution obj = new Solution();
+            int res = obj.countPairs(arr, target);
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
