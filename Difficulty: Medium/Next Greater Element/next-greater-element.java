69cf0344810f180cbcae6bcad63d240921945fd2
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2025-02-23 21:11:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            ArrayList<Integer> result = new Solution().nextLargerElement(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();
+                System.out.println("~");
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the next greater element for each element of the array.
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        ArrayList<Integer> list = new ArrayList<>() ; 
+        for(int i = 0; i<arr.length-1 ; i++){
+            int size = list.size() ; 
+            for(int j = i ; j<arr.length  ; j++){
+                if(arr[i] < arr[j]){
+                    list.add(arr[j]) ; 
+                    break  ; 
+                }
+            }
+            if(size == list.size()){
+                list.add(-1) ; 
+            }
+        }
+        list.add(-1) ; 
+        return list ; 
+    }
+}
