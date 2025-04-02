d7eda6f1fe6d1ecb3ba1da8624ecbdb75ad88478
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2025-04-02 21:39:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+class Solution {
+    // Function to return Breadth First Search Traversal of given graph.
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int n = adj.size();
+        ArrayList<Integer> res = new ArrayList<>();
+        boolean[] vis = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.add(0);
+        vis[0] = true;
+        
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            res.add(node);
+            
+            for (int it : adj.get(node)) {
+                if (!vis[it]) {
+                    q.add(it);
+                    vis[it] = true;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            int V = Integer.parseInt(br.readLine().trim());
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                String[] input = br.readLine().trim().split(" ");
+                ArrayList<Integer> node = new ArrayList<>();
+                for (String s : input) {
+                    if (!s.isEmpty()) {
+                        node.add(Integer.parseInt(s));
+                    }
+                }
+                adj.add(node);
+            }
+
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfs(adj);
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
