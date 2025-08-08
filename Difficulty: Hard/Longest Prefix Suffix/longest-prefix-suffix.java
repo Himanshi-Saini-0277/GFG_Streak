d289f8f24a66360e112ee1244616883dd527f445
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2025-08-08 17:40:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.java
@@ -0,0 +1,26 @@
+class Solution {
+    int getLPSLength(String s) {
+        // code here
+        int n = s.length();
+        int[] lps = new int[n];
+        int len = 0; 
+        int i = 1;
+
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps[n - 1];
+    }
+}
