2bc0da8060afc0481e5b25c87fb90a631e56f323
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2025-08-18 12:03:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        // code here
+        Arrays.sort(citations);
+         int hIndex=0;
+        
+        for(int i=0;i<citations.length;i++){
+           if(citations.length-i<=citations[i]){
+            hIndex=Math.max(hIndex,citations.length-i);
+        }
+        }
+        
+        return hIndex;
+    }
+}
