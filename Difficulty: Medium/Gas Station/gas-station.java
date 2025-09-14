e41db70c6b031d460a1b50fef34f8623f700542f
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2025-09-14 11:50:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.java
+++ Difficulty: Medium/Gas Station/gas-station.java
@@ -1,72 +1,18 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            ArrayList<Integer> array1 = new ArrayList<Integer>();
-            ArrayList<Integer> array2 = new ArrayList<Integer>();
-
-            String line = read.readLine();
-            String[] tokens = line.split(" ");
-            for (String token : tokens) {
-                array1.add(Integer.parseInt(token));
-            }
-            line = read.readLine();
-            tokens = line.split(" ");
-            for (String token : tokens) {
-                array2.add(Integer.parseInt(token));
-            }
-
-            // ArrayList<Integer> v = new ArrayList<Integer>();
-            int[] gas = new int[array1.size()];
-            int idx = 0;
-            for (int i : array1) gas[idx++] = i;
-
-            int[] cost = new int[array2.size()];
-            idx = 0;
-            for (int i : array2) cost[idx++] = i;
-
-            int ans = new Solution().startStation(gas, cost);
-
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public int startStation(int[] gas, int[] cost) {
-        // Your code here
-        int n = gas.length;
-        int totalgas = 0;
-        int totalcost = 0;
-        for(int i=0;i<n;i++){
-            totalgas=totalgas+gas[i];
-            totalcost=totalcost+cost[i];
-        }
-        if(totalgas<totalcost) return -1;
-        int currgas = 0;
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            currgas = currgas+(gas[i]-cost[i]);
-            if(currgas<0){
-                currgas=0;
-                ans = i+1;
+        // code here
+        int tg = 0;
+        int cg = 0;
+        int idx = 0;
+        
+        for(int i = 0; i < gas.length; i ++) {
+            tg += gas[i] - cost[i];
+            cg += gas[i] - cost[i];
+            if(cg < 0) {
+                cg = 0;
+                idx = i + 1;
             }
         }
-        return ans;
+        return tg >= 0 ? idx : -1;
     }
 }
