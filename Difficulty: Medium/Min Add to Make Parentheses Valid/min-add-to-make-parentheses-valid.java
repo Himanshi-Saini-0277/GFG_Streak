aa96877823bf79c73c61393b47aceb2c3a4cc13d
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2025-09-19 19:43:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Add to Make Parentheses Valid/min-add-to-make-parentheses-valid.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minParentheses(String s) {
+        // code here
+        Stack<Character> st = new Stack<>();
+        
+        for(char ch : s.toCharArray()) {
+            if(ch == '(') {
+                st.push(ch);
+            } else {
+                if(!st.isEmpty() && st.peek() == '(') {
+                    st.pop();
+                } else {
+                    st.push(ch);
+                }
+            }
+        }
+        return st.size();
+    }
+}
