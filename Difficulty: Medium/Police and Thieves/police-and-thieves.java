fb03fdbc1a406424f2d3140552fb797bb6eb8753
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2026-01-14 20:25:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Police and Thieves/police-and-thieves.java
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.java
@@ -1,20 +1,25 @@
 class Solution {
     public int catchThieves(char[] arr, int k) {
         // code here
-        Queue<Integer> police=new LinkedList<>(),thieves=new LinkedList<>();
-        int caught=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]=='P') police.offer(i);
-            else thieves.offer(i);
-        }
-        while(!police.isEmpty() && !thieves.isEmpty()){
-            if(Math.abs(police.peek()-thieves.peek())<=k){
-                caught++;
-                police.poll();
-                thieves.poll();
-            }else if(police.peek()<thieves.peek()) police.poll();
-            else thieves.poll();
-        }
-        return caught;
+        int p =0 ; 
+        int t = 0 ; 
+        int count = 0 ; 
+        while(p<arr.length && t<arr.length)
+        {
+            while(p<arr.length && arr[p]!='P') p++;
+            while(t<arr.length && arr[t]!='T') t++;
+             if(p>=arr.length || t>=arr.length ) break ; 
+             
+             if(Math.abs(p-t)<=k)
+             {
+                 count++;
+                 arr[p]='C';
+                 arr[t]='C';
+             }
+             else if(p<t) p++;
+             else t++;
+             
+         }
+         return count ; 
     }
 }
