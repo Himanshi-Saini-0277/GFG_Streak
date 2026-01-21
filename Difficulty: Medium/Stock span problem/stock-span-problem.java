34acd35e99808362d923ffa0008bc78ac2346f36
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2026-01-21 20:08:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.java
+++ Difficulty: Medium/Stock span problem/stock-span-problem.java
@@ -1,63 +1,21 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            ArrayList<Integer> array1 = new ArrayList<Integer>();
-            String line = read.readLine();
-            String[] tokens = line.split(" ");
-            for (String token : tokens) {
-                array1.add(Integer.parseInt(token));
-            }
-            ArrayList<Integer> v = new ArrayList<Integer>();
-            int[] arr = new int[array1.size()];
-            int idx = 0;
-            for (int i : array1) arr[idx++] = i;
-
-            v = new Solution().calculateSpan(arr);
-
-            for (int i = 0; i < v.size(); i++) System.out.print(v.get(i) + " ");
-
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
     public ArrayList<Integer> calculateSpan(int[] arr) {
-        // write code here
-        Stack<Integer>s=new Stack<>();
-        ArrayList<Integer> ans=new ArrayList<Integer>();
-        for(int i=0;i<arr.length;i++)
-        {
-           
-            while(!s.isEmpty()&&arr[s.peek()]<=arr[i])
-            {
-                s.pop();
+        // code here
+        int n = arr.length;
+        ArrayList<Integer> span = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+                stack.pop();
             }
-            if(!s.isEmpty())
-            {
-                ans.add(i-s.peek());
+            if (stack.isEmpty()) {
+                span.add(i + 1);
+            } else {
+                span.add(i - stack.peek());
             }
-            else{
-                ans.add(i+1);
-            }
-            s.add(i);
+            stack.push(i);
         }
-        return ans;
+        return span;
     }
 }
