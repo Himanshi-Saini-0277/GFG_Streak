32bd62b0cca16a547735bbf02706c83db2825fde
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2026-03-12 20:35:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.java
+++ Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.java
@@ -1,23 +1,25 @@
 class Solution {
     public int kBitFlips(int[] arr, int k) {
         // code here
-        int n = arr.length;
-        int[] flipped = new int[n]; 
-        int flip = 0;               
-        int operations = 0;
-
-        for (int i = 0; i < n; i++) {
-           
-            if (i >= k) {
-                flip ^= flipped[i - k];
+        Queue<Integer> qu= new LinkedList<>();
+        int n=arr.length;
+        int f=0;
+        for(int i=0;i<n;i++){
+            if(!qu.isEmpty() && qu.peek()+k==i){
+                qu.poll();
             }
-                if ((arr[i] ^ flip) == 0) {
-                if (i + k > n) return -1; 
-                operations++;
-                flip ^= 1;         
-                flipped[i] = 1;    
+            int cr=arr[i];
+            if(qu.size()%2!=0){
+                cr^=1;
+            }
+            if(cr==0){
+                if(i+k>n){
+                    return -1;
+                }
+                qu.offer(i);
+                f++;
             }
         }
-        return operations;
+        return f;
     }
 }
