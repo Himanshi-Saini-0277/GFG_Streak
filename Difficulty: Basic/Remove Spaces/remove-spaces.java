df38c494e288d3c36e9e158deb4579d453217f8c
Himanshi <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2026-04-14 09:03:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.java
@@ -0,0 +1,9 @@
+class Solution {
+    String removeSpaces(String s) {
+        // code here
+        String str = s.replaceAll(" ","");
+        //   System.out.print(str);
+        return str;
+
+    }
+}
